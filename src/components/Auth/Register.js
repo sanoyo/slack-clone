fe65762e4ca6dc488ad17d08447ad67a131e6734
sanoyo <sa00380304yo@gmail.com>
2020-08-27 09:17:40 +0900

    ユーザー登録実装

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,4 +1,6 @@
 import React from "react";
+import firebase from "../../firebase";
+
 import {
   Grid,
   Form,
@@ -11,11 +13,34 @@ import {
 import { Link } from "react-router-dom";
 
 class Register extends React.Component {
-  state = {};
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  };
 
-  handleChange = () => {};
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    // リンク先への遷移を抑制
+    event.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(createdUser => {
+        console.log(createdUser);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
 
   render() {
+    const { username, email, password, passwordConfirmation } = this.state;
+    
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -23,7 +48,7 @@ class Register extends React.Component {
             <Icon name="puzzle piece" color="orange" />
             Register for DevChat
           </Header>
-          <Form size="large">
+          <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
               <Form.Input
                 fluid
@@ -32,6 +57,7 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Username"
                 onChange={this.handleChange}
+                value={username}
                 type="text"
               />
 
@@ -42,6 +68,7 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Email Address"
                 onChange={this.handleChange}
+                value={email}
                 type="email"
               />
 
@@ -52,6 +79,7 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Password"
                 onChange={this.handleChange}
+                value={password}
                 type="password"
               />
 
@@ -62,6 +90,7 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Password Confirmation"
                 onChange={this.handleChange}
+                value={passwordConfirmation}
                 type="password"
               />
 
