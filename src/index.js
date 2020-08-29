dc9fd2c8babf12d524c5f349c22fb0fc590bbd02
sanoyo <sa00380304yo@gmail.com>
2020-08-29 15:31:11 +0900

    redux 実装

--- src/index.js
+++ src/index.js
@@ -9,11 +9,20 @@ import firebase from './firebase';
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         console.log(user);
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     })
@@ -32,12 +41,15 @@ class Root extends React.Component {
   };
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth /> 
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth /> 
+    </Router>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 registerServiceWorker();
